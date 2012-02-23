f0f85177bcb0cb684ad543d0e3fb1095245c9547
Alessandro Motta <amotta91@gmail.com>
2012-02-23 22:00:49 +0100

    Iterate over arguments in main function

--- main.c
+++ main.c
@@ -3,7 +3,27 @@
 //  StickyParticles
 //
 //  Created by Alessandro Motta on 2/23/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+static void usage(){
+    printf(
+        "Usage of Sticky Particles\n"
+        " sticky.x file1 [file2 file3 ... fileN]\n"
+    );
+}
+
+int main(int argc, char** argv){
+    if(argc < 2){
+        usage();
+        return EXIT_SUCCESS;
+    }
+
+    while(++argv, --argc){
+        printf("%s\n", *argv);
+    }
+    
+    return EXIT_SUCCESS;
+}
