11786aa987230b6a033f29532112bc5ae38fe3a0
Alessandro Motta <amotta91@gmail.com>
2012-02-23 21:24:01 +0100

    Add constants.h taken from PDF file

---
+++ constants.h
@@ -0,0 +1,10 @@
+// constants.h
+
+#define RECT_X 8.f // m
+#define RECT_Y 6.f // m
+#define R_DISK 0.2f // m
+#define R_PART 0.1f // m
+#define MIN_VG 0.1f // m/s
+#define MAX_VG 1.f // m/s
+#define MAX_ALPHA 0.7f // rd
+#define MAX_VDISK (3*MAX_VG) // m/s
