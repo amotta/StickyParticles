0a2b7bf1e6150854a919fce118fec5b82cd26376
Alessandro Motta <amotta91@gmail.com>
2012-02-23 22:00:19 +0100

    Populate makefile with targets

---
+++ main.c
@@ -0,0 +1,9 @@
+//
+//  main.c
+//  StickyParticles
+//
+//  Created by Alessandro Motta on 2/23/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
--- makefile
+++ makefile
@@ -1,1 +1,12 @@
 # makefile
+
+CC = gcc
+FLAGS = -Wall -pedantic
+
+all: sticky.x
+
+sticky.x: main.o
+	$(CC) $(FLAGS) $< -o $@
+
+main.o: main.c
+	$(CC) $(FLAGS) -c $< -o $@
