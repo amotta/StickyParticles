124ef3d252b270c20944f1f938ef321a708944e6
Alessandro Motta <amotta91@gmail.com>
2012-02-24 15:19:40 +0100

    Introduce gameFileReadScore function

--- game.c
+++ game.c
@@ -9,15 +9,41 @@
 
 #include "game.h"
 
+static int gameFileReadScore(FILE* file){
+    unsigned int score;
+    while(fscanf(file, "%ud", &score) < 1){
+        // gameFileSkip(file);
+    }
+    
+    return 0;
+}
+
+static int gameFileParse(FILE* file){
+    char buf[BUFSIZE];
+    
+    clearerr(file);
+    while(fgets(buf, BUFSIZE, file)){
+        // analyze content
+    }
+    
+    if(ferror(file)){
+        printf("ERROR: Error while reading file\n");
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
 extern int gameFileRead(char* name){
     FILE* file = NULL;
-    char buf[BUFSIZE];
     
     file = fopen(name, "r");
     if(!file){
         printf("ERROR: Could not open file\n");
         return 1;
     }
+    
+    gameFileParse(file);
     
     fclose(file);
     
--- game.h
+++ game.h
@@ -5,6 +5,10 @@
 //  Created by Alessandro Motta on 2/23/12.
 //
 
+#include <stdio.h>
+
 #define BUFSIZE 255
 
+static int gameFileReadScore(FILE* file);
+static int gameFileParse(FILE* file);
 extern int gameFileRead(char* name);
