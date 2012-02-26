423878f65fa4a840fdd36a269fc53bf2a64aae60
Alessandro Motta <amotta91@gmail.com>
2012-02-26 10:52:54 +0100

    Correct little type in constants.h

--- constants.h
+++ constants.h
@@ -10,7 +10,7 @@
 
 #define RECT_X			8.f		//m 
 #define RECT_Y			6.f 	//m
-#define R_DISK			0.2f	//m
+#define R_DISC			0.2f	//m
 #define R_PART			0.1f	//m
 #define MIN_VG			0.1f	//m/s
 #define MAX_VG			1.f		//m/s
