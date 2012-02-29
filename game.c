275e28f2c671fb2d9797157b993f682747b5a975
Alessandro Motta <amotta91@gmail.com>
2012-02-29 18:24:18 +0100

    Write functions for lecture of groups

--- game.c
+++ game.c
@@ -7,6 +7,7 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "game.h"
 #include "main.h"
@@ -17,6 +18,9 @@ static int gameFileReadInterval(FILE* file);
 static int gameFileReadDisc(FILE* file);
 static int gameFileReadEmitter(FILE* file);
 static int gameFileReadEmitters(FILE* file);
+static int gameFileReadGroupType(FILE* file);
+static int gameFileReadGroup(FILE* file);
+static int gameFileReadGroups(FILE* file);
 
 static int gameFileSkip(FILE* file){
     char c;
@@ -173,7 +177,91 @@ static int gameFileReadEmitters(FILE* file){
     return OK;
 }
 
+static int gameFileReadGroupType(FILE* file){
+    int error = OK;
+    
+    if(error = gameFileSkip(file)){
+        return error;
+    }
+    
+    char buf[11];
+    if(fscanf(file, "%10s", buf) < 1){
+        return GROUP_TYPE_NONE;
+    }
+    
+    if(!strcmp(buf, "INOFFENSIF")){
+        return GROUP_TYPE_HARMLESS;
+    }else if(!strcmp(buf, "DANGEREUX")){
+        return GROUP_TYPE_DANGEROUS;
+    }else{
+        return GROUP_TYPE_NONE;
+    }
+}
 
+static int gameFileReadGroup(FILE* file){
+    int error = OK;
+    double x, y, vx, vy, omega;
+    int type;
+    unsigned int numbParts;
+    
+    if(error = gameFileSkip(file)){
+        printf("ERROR 208\n");
+        return error;
+    }
+    
+    if(fscanf(file, "%lf %lf %lf %lf %lf", &x, &y, &vx, &vy, &omega) < 5){
+        printf("ERROR 213\n");
+        return GAME_ERROR_FORMAT;
+    }
+    
+    if((type = gameFileReadGroupType(file)) == GROUP_TYPE_NONE){
+        // TODO proper error message
+        printf("ERROR 219\n");
+        return GAME_ERROR_FORMAT;
+    }
+    
+    if(fscanf(file, "%u", &numbParts) < 1){
+        printf("ERROR 224\n");
+        return GAME_ERROR_FORMAT;
+    }
+    
+    // DEBUG
+    printf(
+        " X: %f, Y: %f, VX: %f, VY: %f, OMEGA: %f, TYPE: %s, PARTS COUNT: %u\n",
+        x, y,
+        vx, vy,
+        omega,
+        type == GROUP_TYPE_HARMLESS ? "HARMLESS" : "DANGEROUS",
+        numbParts
+    );
+    
+    return OK;
+}
+
+static int gameFileReadGroups(FILE* file){
+    int error = OK;
+    unsigned int numbGroups;
+    
+    if(error = gameFileSkip(file)){
+        return error;
+    }
+    
+    if(fscanf(file, "%u", &numbGroups) < 1){
+        return GAME_ERROR_FORMAT;
+    }
+    
+    // DEUBG
+    printf("Group count: %u\n", numbGroups);
+    
+    unsigned int i;
+    for(i = 0; i < numbGroups; i++){
+        if(error = gameFileReadGroup(file)){
+            return error;
+        }
+    }
+    
+    return OK;
+}
 
 extern int gameFileRead(char* name){
     int error = OK;
@@ -188,6 +276,7 @@ extern int gameFileRead(char* name){
     error = error || gameFileReadInterval(file);
     error = error || gameFileReadDisc(file);
     error = error || gameFileReadEmitters(file);
+    error = error || gameFileReadGroups(file);
     
     fclose(file);
     
--- game.h
+++ game.h
@@ -15,6 +15,11 @@
 #define GAME_ERROR_FOPEN 3
 #define GAME_ERROR_NO_SEPA 4
 
+// TODO: move to group.h
+#define GROUP_TYPE_NONE 0
+#define GROUP_TYPE_HARMLESS 1
+#define GROUP_TYPE_DANGEROUS 2
+
 extern int gameFileRead(char* name);
 
 #endif
