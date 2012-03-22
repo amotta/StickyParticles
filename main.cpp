088e40e0994278d761287c00a43bceb580162e20
Alessandro Motta <amotta91@gmail.com>
2012-03-22 16:29:41 +0100

    Add option to switch between verbose and silent mode.

--- main.cpp
+++ main.cpp
@@ -5,37 +5,66 @@
 //  Created by Alessandro Motta on 2/23/12.
 //
 
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 extern "C" {
+    #include <stdbool.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+
     #include "game.h"
 }
 
 static void usage();
+static bool isOpt(char* arg);
+static void handleOpt(char* arg);
+static void handleFile(char* arg);
 
 int main(int argc, char** argv){
+    int i;
+    
     if(argc < 2){
         usage();
         return EXIT_SUCCESS;
     }
     
-    gameSetDebug(true);
-    
-    int i;
     for(i = 1; i < argc; i++){
-        if(!gameFileRead(argv[i])){
-            gamePrintError();
+        if(isOpt(argv[i])){
+            handleOpt(argv[i]);
+        }else{
+            handleFile(argv[i]);
         }
     }
     
     return EXIT_SUCCESS;
 }
 
+static bool isOpt(char* arg){
+    if(arg[0] == '-'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+static void handleOpt(char* arg){
+    if(!strcmp("-v", arg)){
+        gameSetDebug(true);
+    }else if(!strcmp("-s", arg)){
+        gameSetDebug(false);
+    }
+}
+
+static void handleFile(char* file){
+    if(!gameFileRead(file)){
+        gamePrintError();
+    }
+}
+
 static void usage(){
     printf(
         "Usage of Sticky Particles\n"
-        " sticky.x file1 [file2 file3 ... fileN]\n"
+        " sticky.x file1 [file2 ... fileN]\n"
+        " -v    Activate debugging\n"
+        " -s    Deactivate debugging\n"
     );
 }
