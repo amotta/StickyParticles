9615f507f79ac9d47999b00bcd3d32e93ab8a651
Alessandro Motta <amotta91@gmail.com>
2012-04-10 10:48:31 +0200

    New module file

---
+++ file.c
@@ -0,0 +1,494 @@
+//
+//  file.c
+//  StickyParticles
+//
+//  Created by Alessandro Motta on 4/10/12.
+//
+
+
+#define LINE_BUF_LEN 81
+
+static bool fileSkip();
+static bool fileReadLine(char* buf, int bufLen);
+static bool fileReadScore();
+static bool fileReadInterval();
+static bool fileReadDisc();
+static bool fileReadEmitter();
+static bool fileReadEmitters();
+static bool fileReadGroup();
+static bool fileReadGroups();
+static bool fileReadPart();
+static void fileSetError(int errorCode);
+static void filePrintStatus();
+
+static int error;
+static bool debug;
+static FILE* file;
+static char* fileName;
+static unsigned int lineNumber;
+
+enum FILE_ERROR_CODES {
+    FILE_OK,
+    FILE_ERROR_EOF,
+    FILE_ERROR_FOPEN,
+    FILE_ERROR_FGETS,
+    FILE_ERROR_LINE_TOO_LONG,
+    FILE_ERROR_SCORE,
+    FILE_ERROR_INTERVAL,
+    FILE_ERROR_INTERVAL_NEGATIVE,
+    FILE_ERROR_DISC,
+    FILE_ERROR_DISC_POS,
+    FILE_ERROR_EMITTER,
+    FILE_ERROR_EMITTER_ALPHA,
+    FILE_ERROR_EMITTER_POS,
+    FILE_ERROR_EMITTER_SPEED,
+    FILE_ERROR_EMITTERS,
+    FILE_ERROR_GROUPTYPE,
+    FILE_ERROR_GROUP,
+    FILE_ERROR_GROUP_SPEED,
+    FILE_ERROR_GROUPS,
+    FILE_ERROR_PART,
+    FILE_ERROR_PART_POS,
+    FILE_ERROR_FCLOSE
+};
+
+static char* FILE_ERROR_MESSAGES[] = {
+    "No error",
+    "Reached end of file",
+    "Could not open file",
+    "Could not read line from file",
+    "Line is too long",
+    "Could not read score",
+    "Could not read interval",
+    "Time interval is negative",
+    "Could not read disc",
+    "Position of disc is invalid",
+    "Could not read emitter",
+    "Emitter angle is invalid",
+    "Position of emitter is invalid",
+    "Speed of emitter is invalid",
+    "Could not read number of emitters",
+    "Could not read group type",
+    "Could not read group",
+    "Speed of group is invalid",
+    "Could not read number of groups",
+    "Could not read particle",
+    "Position of particle is invalid",
+    "Could not close file"
+};
+
+// This function skips comments and whitespaces
+// Muchas gracias to git for inspiration
+static bool fileSkip(){
+    char c;
+    bool comment = false;
+    
+    do{
+        c = fgetc(file);
+        
+        switch(c){
+            case ' ':
+            case '\t':
+                continue;
+                
+            case '#':
+                comment = true;
+                continue;
+                
+            case '\r':
+                c = fgetc(file);
+                
+                // aight, it's Windows
+                if(c != '\n'){
+                    ungetc(c, file);
+                }
+                
+            case '\n':
+                comment = false;
+                lineNumber++;
+                continue;
+                
+            case EOF:
+                fileSetError(FILE_ERROR_EOF);
+                return false;
+                
+            default:
+                if(!comment){
+                    ungetc(c, file);
+                    return true;
+                }
+        }
+    }while(true);
+}
+
+static bool fileReadLine(char* buf, int bufLen){
+    if(!fileSkip()){
+        return false;
+    }
+    
+    // level up
+    lineNumber++;
+    
+    if(fgets(buf, bufLen, file) == NULL){
+        fileSetError(FILE_ERROR_FGETS);
+        return false;
+    }
+    
+    // check if we had enough space for the entire line
+    if(buf[strlen(buf) - 1] != '\n'){
+        fileSetError(FILE_ERROR_LINE_TOO_LONG);
+        return false;
+    }
+    
+    // seach for comment sign
+    char* comment;
+    if((comment = strstr(buf, "#"))){
+        *comment = '\0';
+    }
+    
+    return true;
+}
+
+static bool fileReadScore(){
+    char line[LINE_BUF_LEN];
+    unsigned int score;
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(sscanf(line, "%u", &score) < 1){
+        fileSetError(FILE_ERROR_SCORE);
+        return false;
+    }
+    
+    if(debug){
+        printf("Score: %u\n", score);
+    }
+    
+    return true;
+}
+
+static bool fileReadInterval(){
+    char line[LINE_BUF_LEN];
+    double interval;
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(sscanf(line, "%lf", &interval) < 1){
+        fileSetError(FILE_ERROR_INTERVAL);
+        return false;
+    }
+    
+    if(interval < 0){
+        fileSetError(FILE_ERROR_INTERVAL_NEGATIVE);
+        return false;
+    }
+    
+    if(debug){
+        printf("Interval: %f\n", interval);
+    }
+    
+    return true;
+}
+
+static bool fileReadDisc(){
+    char line[LINE_BUF_LEN];
+    circ_t disc = {
+        .r = R_DISC
+    };
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(sscanf(line, "%lf %lf", &disc.pos.x, &disc.pos.y) < 2){
+        fileSetError(FILE_ERROR_DISC);
+        return false;
+    }
+    
+    if(debug){
+        printf(
+               "Disc\n"
+               " X: %f, Y: %f\n", disc.pos.x, disc.pos.y
+               );
+    }
+    
+    if(!isCircInGameCirc(disc)){
+        fileSetError(FILE_ERROR_DISC_POS);
+        return false;
+    }
+    
+    return true;
+}
+
+static bool fileReadEmitter(){
+    char line[LINE_BUF_LEN];
+    vect_t pos;
+    double alpha, flow, speed;
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(
+        sscanf(
+            line,
+            "%lf %lf %lf %lf %lf",
+            &pos.x, &pos.y, &alpha, &flow, &speed
+        ) < 5
+    ){
+        fileSetError(FILE_ERROR_EMITTER);
+        return false;
+    }
+    
+    if(debug){
+        printf(
+            " X: %f, Y: %f, Alpha: %f, Flow: %f, Speed: %f\n",
+            pos.x, pos.y, alpha, flow, speed
+        );
+    }
+    
+    // validation
+    if(alpha > MAX_ALPHA){
+        fileSetError(FILE_ERROR_EMITTER_ALPHA);
+        return false;
+    }
+    
+    if(speed > MAX_VG || MIN_VG > speed){
+        fileSetError(FILE_ERROR_EMITTER_SPEED);
+        return false;
+    }
+    
+    if(isVectInGameCirc(pos) || !isVectInGameRect(pos)){
+        fileSetError(FILE_ERROR_EMITTER_POS);
+        return false;
+    }
+    
+    return true;
+}
+
+static bool fileReadEmitters(){
+    char line[LINE_BUF_LEN];
+    unsigned int numbEmitters;
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(sscanf(line, "%u", &numbEmitters) < 1){
+        fileSetError(FILE_ERROR_EMITTERS);
+        return false;
+    }
+    
+    if(debug){
+        printf("Emitter count: %u\n", numbEmitters);
+    }
+    
+    // read emitters
+    unsigned int i;
+    for(i = 0; i < numbEmitters; i++){
+        if(!fileReadEmitter()){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+static bool fileReadGroup(){
+    char buf[11];
+    char line[LINE_BUF_LEN];
+    
+    vect_t pos;
+    vect_t speed;
+    double omega;
+    unsigned int type, numbParts;
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(
+        sscanf(
+            line,
+            "%lf %lf %lf %lf %lf %10s %u",
+            &pos.x, &pos.y, &speed.x, &speed.y, &omega, buf, &numbParts
+        ) < 7
+    ){
+        fileSetError(FILE_ERROR_GROUP);
+        return false;
+    }
+    
+    // check for valid values
+    if(!strcmp(buf, "INOFFENSIF")){
+        type = GROUP_TYPE_HARMLESS;
+    }else if(!strcmp(buf, "DANGEREUX")){
+        type = GROUP_TYPE_DANGEROUS;
+    }else{
+        fileSetError(FILE_ERROR_GROUPTYPE);
+        return false;
+    }
+    
+    if(debug){
+        printf(
+            " X: %f, Y: %f, VX: %f, VY: %f,"
+            " OMEGA: %f,"
+            " TYPE: %s,"
+            " PARTS COUNT: %u\n",
+            pos.x, pos.y, speed.x, speed.y,
+            omega,
+            type == GROUP_TYPE_HARMLESS ? "HARMLESS" : "DANGEROUS",
+            numbParts
+        );
+    }
+    
+    // validate speed
+    double speedLen = vectLength(speed);
+    if(speedLen > MAX_VG || speedLen < MIN_VG){
+        fileSetError(FILE_ERROR_GROUP_SPEED);
+        return false;
+    }
+    
+    // read particles
+    if(numbParts > 1){
+        unsigned int i;
+        for(i = 0; i < numbParts; i++){
+            if(!fileReadPart()){
+                return false;
+            }
+        }
+    }else{
+        circ_t part = {
+            .pos = pos,
+            .r = R_PART
+        };
+        
+        if(!isCircInGameRect(part)){
+            fileSetError(FILE_ERROR_PART_POS);
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+static bool fileReadGroups(){
+    char line[LINE_BUF_LEN];
+    unsigned int numbGroups;
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(sscanf(line, "%u", &numbGroups) < 1){
+        error = FILE_ERROR_GROUPS;
+        return false;
+    }
+    
+    if(debug){
+        printf("Group count: %u\n", numbGroups);
+    }
+    
+    unsigned int i;
+    for(i = 0; i < numbGroups; i++){
+        if(!fileReadGroup()){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+static bool fileReadPart(){
+    char line[LINE_BUF_LEN];
+    circ_t part = {
+        .r = R_PART
+    };
+    
+    if(!fileReadLine(line, LINE_BUF_LEN)){
+        return false;
+    }
+    
+    if(sscanf(line, "%lf %lf", &part.pos.x, &part.pos.y) < 2){
+        fileSetError(FILE_ERROR_PART);
+        return false;
+    }
+    
+    if(debug){
+        printf("  X: %lf, Y: %lf\n", part.pos.x, part.pos.y);
+    }
+    
+    if(!isCircInGameRect(part)){
+        fileSetError(FILE_ERROR_PART_POS);
+        return false;
+    }
+    
+    return true;
+}
+
+void fileSetError(int errorCode){
+    error = errorCode;
+}
+
+void filePrintStatus(){
+    printf("FILE %s\n", fileName);
+    
+    if(error){
+        printf(
+            " ERROR on line %u: %s\n", lineNumber, GAME_ERROR_MESSAGES[error]
+        );
+    }else{
+        printf(" VALID\n");
+    }
+}
+
+bool fileRead(char* name){
+    bool ok = true;
+    
+    error = FILE_OK;
+    fileName = name;
+    file = fopen(fileName, "r");
+    lineNumber = 0;
+    
+    if(debug){
+        printf(
+            "\n"
+            "File: %s\n", fileName
+        );
+    }
+    
+    if(!file){
+        ok = false;
+        fileSetError(FILE_ERROR_FOPEN);
+    }
+    
+    ok = ok && fileReadScore();
+    ok = ok && fileReadInterval();
+    ok = ok && fileReadDisc();
+    ok = ok && fileReadEmitters();
+    ok = ok && fileReadGroups();
+    
+    if(file && fclose(file)){
+        fileSetError(FILE_ERROR_FCLOSE);
+    }
+    
+    if(debug){
+        printf("\n");
+    }
+    
+    gamePrintStatus();
+    
+    if(error){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+void fileSetDebug(bool flag){
+    debug = flag;
+}
+
---
+++ file.h
@@ -0,0 +1,14 @@
+//
+//  file.h
+//  StickyParticles
+//
+//  Created by Alessandro Motta on 4/10/12.
+//
+
+#ifndef FILE_H
+#define FILE_H
+
+extern bool fileRead(char* name);
+extern void fileSetDebug(bool flag);
+
+#endif
