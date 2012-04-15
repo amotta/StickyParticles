6f024fae8c79bf2c408da0dcca62c915c74630eb
Alessandro Motta <amotta91@gmail.com>
2012-04-15 21:03:40 +0200

    Move all geometry validations to Geometry module

--- geometry.c
+++ geometry.c
@@ -6,32 +6,138 @@
 //
 
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "circle.h"
+#include "constants.h"
 #include "geometry.h"
+#include "particle.h"
 #include "rectangle.h"
+#include "vector.h"
 
-// TODO
-/*
-bool isCircInRect(circ_t circ, rect_t rect){
+bool isCircInRect(circ_t* circ, rect_t* rect){
+    static rect_t* rectInt;
+    bool valid = false;
+    double radius;
+    
+    if(!circ || !rect) return;
+    
+    if(!rectInt){
+        rectInt = rectNew();
+    }
+    
+    // get radius
+    radius = circGetRadius(circ);
+    
     // hiiiiigh power!
     if(
-       rect.right - rect.left < circ.r
-       || rect.top - rect.bottom < circ.r
+       rectGetRight(rect) - rectGetLeft(rect) < radius
+       || rectGetTop(rect) - rectGetBottom(rect) < radius
     ){
         return false;
     }
     
-    rect_t zone = rect;
-    zone.left += circ.r;
-    zone.right -= circ.r;
-    zone.bottom += circ.r;
-    zone.top -= circ.r;
+    // set up rect
+    rectSetLeft(rectInt, rectGetLeft(rect) + radius);
+    rectSetRight(rectInt, rectGetRight(rect) - radius);
+    rectSetBottom(rectInt, rectGetBottom(rect) + radius);
+    rectSetTop(rectInt, rectGetTop(rect) - radius);
     
-    return isVectInRect(circ.pos, zone);
+    return isVectInRect(circGetPos(circ), rectInt);
 }
 
-bool isCircInGameRect(circ_t circ){
-    return isCircInRect(circ, gameRect);
+bool isCircInGameRect(circ_t* circ){
+    return isCircInRect(circ, getGameRect());
 }
-*/
+
+bool isPartInGameCirc(part_t* part){
+    static circ_t* circ;
+    bool valid = false;
+    
+    if(!part) return;
+    
+    if(!circ){
+        circ = circNew();
+        circSetRadius(circ, R_PART);
+    }
+    
+    // set pos
+    circSetPos(circ, partGetPos(part));
+    
+    // check
+    valid = isCircInGameCirc(circ);
+    
+    // unset pos
+    circSetPos(circ, NULL);
+    
+    return valid;
+}
+
+bool isPartInGameRect(part_t* part){
+    static circ_t* circ;
+    bool valid = false;
+    
+    if(!part) return;
+    
+    if(!circ){
+        circ = circNew();
+        circSetRadius(circ, R_PART);
+    }
+    
+    // set pos
+    circSetPos(circ, partGetPos(part));
+    
+    // check
+    valid = isCircInGameRect(circ);
+    
+    // unset pos
+    circSetPos(circ, NULL);
+    
+    return valid;
+}
+
+bool isVectInCirc(vect_t* vect, circ_t* circExt){
+    static circ_t* circInt;
+    bool valid = false;
+    
+    if(!vect || !circExt) return;
+    
+    if(!circInt){
+        circInt = circNew();
+        circSetRadius(circInt, 0);
+    }
+    
+    // set circle position
+    circSetPos(circInt, vect);
+    
+    // check
+    valid = isCircInCirc(circInt, circExt);
+    
+    // unset pos
+    circSetPos(circInt, NULL);
+    
+    return valid;
+}
+
+bool isVectInGameCirc(vect_t* vect){
+    return isVectInCirc(vect, getGameCirc());
+}
+
+bool isVectInRect(vect_t* vect, rect_t* rect){
+    if(!vect || !rect) return false;
+    
+    if(
+       rectGetLeft(rect) < vectGetX(vect)
+       && vectGetX(vect) < rectGetRight(rect)
+       && rectGetBottom(rect) < vectGetY(vect)
+       && vectGetY(vect) < rectGetTop(rect)
+    ){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isVectInGameRect(vect_t* vect){
+    return isVectInRect(vect, getGameRect());
+}
