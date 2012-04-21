402f45c5aca059e3a9e496a6c417adf28ec0606e
Alessandro Motta <amotta91@gmail.com>
2012-04-21 16:21:25 +0200

    Add all elements to control UI

--- controlui.cpp
+++ controlui.cpp
@@ -9,10 +9,15 @@
 
 #include "controlui.h"
 
-#define EXIT_ID 1
-#define FILE_ID 2
-#define LOAD_ID 3
-#define SAVE_ID 4
+enum UI_IDS {
+    UI_ID_EXIT,
+    UI_ID_FILE,
+    UI_ID_LOAD,
+    UI_ID_SAVE,
+    UI_ID_INTERVAL,
+    UI_ID_STEP,
+    UI_ID_PLAY
+};
 
 namespace {
 	// listeners
@@ -22,6 +27,7 @@ namespace {
 	// GLUI
 	GLUI* glui = NULL;
     GLUI_EditText* fileText = NULL;
+    GLUI_Spinner* deltaSpinner = NULL;
 }
 
 void ctrlUIInit(){
@@ -37,15 +43,35 @@ void ctrlUIInit(){
         "File",
         GLUI_EDITTEXT_TEXT,
         NULL,
-        FILE_ID,
+        UI_ID_FILE,
         ctrlUIHandleEvent
     );
     
-    glui->add_button_to_panel(panel, "Load", LOAD_ID, ctrlUIHandleEvent);
-    glui->add_button_to_panel(panel, "Save", SAVE_ID, ctrlUIHandleEvent);
+    glui->add_button_to_panel(panel, "Load", UI_ID_LOAD, ctrlUIHandleEvent);
+    glui->add_button_to_panel(panel, "Save", UI_ID_SAVE, ctrlUIHandleEvent);
+    
+    // simulation panel
+    panel = glui->add_panel("Simulation");
+    glui->add_spinner_to_panel(
+        panel,
+        "Interval",
+        GLUI_SPINNER_FLOAT,
+        NULL,
+        UI_ID_INTERVAL,
+        ctrlUIHandleEvent
+    );
+    
+    glui->add_button_to_panel(panel, "Step", UI_ID_STEP, ctrlUIHandleEvent);
+    glui->add_button_to_panel(panel, "Play", UI_ID_PLAY, ctrlUIHandleEvent);
+    
+    // information panel
+    panel = glui->add_panel("Informations");
+    glui->add_edittext_to_panel(panel, "Time", GLUI_EDITTEXT_FLOAT);
+    glui->add_edittext_to_panel(panel, "Score", GLUI_EDITTEXT_INT);
+    glui->add_edittext_to_panel(panel, "Status", GLUI_EDITTEXT_TEXT);
     
     // exit button
-	glui->add_button("Exit", EXIT_ID, ctrlUIHandleEvent);
+	glui->add_button("Exit", UI_ID_EXIT, ctrlUIHandleEvent);
 }
 
 void ctrlUISetOnLoad(void (*load)(const char* file)){
@@ -62,15 +88,15 @@ void ctrlUISetGameWindow(int wind){
 
 void ctrlUIHandleEvent(int id){
 	switch(id){
-		case EXIT_ID:
+		case UI_ID_EXIT:
 			if(onExit) onExit();
 			break;
             
-        case LOAD_ID:
+        case UI_ID_LOAD:
             if(onLoad) onLoad(fileText->get_text());
             break;
             
-        case SAVE_ID:
+        case UI_ID_SAVE:
             break;
 	}
 }
