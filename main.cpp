18388bf168d53ea3edca2813c07cc91e95dd2eec
Alessandro Motta <amotta91@gmail.com>
2012-04-24 16:05:09 +0200

    Handle mouse and keyboard events

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@ extern "C" {
 
 bool isOpt(char* arg);
 void handleOpt(char* arg);
+void handleKeyboard(unsigned char key, int x, int y);
+void handleSpecial(int key, int x, int y);
+void handleMouse(int button, int state, int x, int y);
 void usage();
 
 int main(int argc, char** argv){
@@ -33,6 +36,9 @@ int main(int argc, char** argv){
 	
     // set idle listener
     GLUI_Master.set_glutIdleFunc(NULL);
+    GLUI_Master.set_glutKeyboardFunc(handleKeyboard);
+    GLUI_Master.set_glutSpecialFunc(handleSpecial);
+    GLUI_Master.set_glutMouseFunc(handleMouse);
     
     // show usage indications
     usage();
@@ -64,6 +70,21 @@ void handleOpt(char* arg){
     }else if(!strcmp("-s", arg)){
         gameSetDebug(false);
     }
+}
+
+void handleKeyboard(unsigned char key, int x, int y){
+    printf("Key #%d pressed\n", key);
+}
+
+void handleSpecial(int key, int x, int y){
+    printf("Special key #%d pressed \n", key);
+}
+
+void handleMouse(int button, int state, int x, int y){
+    printf(
+        "Mouse key #%d changed to state #%d at X: %d Y: %d\n",
+        button, state, x, y
+    );
 }
 
 void usage(){
